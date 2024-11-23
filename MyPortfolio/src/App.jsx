61a8bf7c97af27006edd7989aa2ef2ce16466335
NorPeK <norpek@hotmail.co.uk>
2024-11-23 18:57:47 +0300

    Client Section

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from './sections/Navbar.jsx'
 import Hero from './Sections/Hero.jsx'
 import About from './Sections/About.jsx'
 import Projects from './Sections/Projects.jsx'
+import Clients from './Sections/Clients.jsx'
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
@@ -9,6 +10,7 @@ const App = () => {
       <Hero />
       <About />
       <Projects />
+      <Clients />
     </main>
   )
 }
--- MyPortfolio/src/Sections/About.jsx
+++ MyPortfolio/src/Sections/About.jsx
@@ -17,7 +17,7 @@ const About = () => {
             <div className="col-span-1 xl:row-span-3">
                 <div className="grid-container">
                     <img src="assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
-                    <div>
+                    <div> 
                         <p className="grid-headtext">Hi, I'm Nour-Allah</p>
                         <p className="grid-subtext">With 12 years of experience, I have honed my skills in frontend and backend development with a focus on 3d development.</p>
                     </div>
