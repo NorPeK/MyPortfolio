edf3925c3b159e11254f05571e8deda50803e216
NorPeK <norpek@hotmail.co.uk>
2024-11-23 16:44:33 +0300

    Projects Section

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import Navbar from './sections/Navbar.jsx'
 import Hero from './Sections/Hero.jsx'
 import About from './Sections/About.jsx'
+import Projects from './Sections/Projects.jsx'
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
       <Hero />
       <About />
+      <Projects />
     </main>
   )
 }
