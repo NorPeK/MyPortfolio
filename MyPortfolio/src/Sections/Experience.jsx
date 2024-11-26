8204d88ac5b888f9a0dc43e29367f1a9d7c69e42
NorPeK <norpek@hotmail.co.uk>
2024-11-27 01:25:41 +0300

    work experience section completed

--- MyPortfolio/src/Sections/Experience.jsx
+++ MyPortfolio/src/Sections/Experience.jsx
@@ -41,7 +41,7 @@ const Experience = () => {
                                 <div className="sm:p-5 px-2.5 py-5">
                                     <p className="font-bold text-white-800">{name}</p>
                                     <p className="text-sm mb-5">{pos} -- {duration}</p>
-                                    <p className="group-hover:text-white trasition ease-in-out duration-500">{title}</p>
+                                    <p className="group-hover:text-white trasition ease-in-out duration-500" style={{ textAlign: "justify" }}>{title}</p>
                                 </div>
                             </div>
                         ))}
