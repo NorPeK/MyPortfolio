f89d6bcd73782fc7018f46952d153dbeefdc1a51
NorPeK <norpek@hotmail.co.uk>
2024-11-27 00:00:17 +0300

    about finished

--- MyPortfolio/src/Sections/About.jsx
+++ MyPortfolio/src/Sections/About.jsx
@@ -5,10 +5,17 @@ import { useState } from "react";
 const About = () => {
 
     const [hasCopied, setHasCopied] = useState(false);
+    const [hasCopiedNumber, setHasCopiedNumber] = useState(false);
     const handleCopy = () => {
         navigator.clipboard.writeText("norpekbusiness@hotmail.com");
         setHasCopied(true);
         setTimeout(() => setHasCopied(false), 2000);
+    }
+
+    const handleCopy2 = () => {
+        navigator.clipboard.writeText("+966 533 199 759");
+        setHasCopiedNumber(true);
+        setTimeout(() => setHasCopiedNumber(false), 2000);
     }
 
   return (
@@ -26,11 +33,14 @@ const About = () => {
 
             <div className="col-span-1 xl:row-span-3">
                 <div className="grid-container">
-                    <img src="assets/grid2.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain"/>
+                    <img src="assets/norpekGroup.png" alt="grid-2" className="w-full sm:h-[276px] h-fit object-contain"/>
                     <div>
-                        <p className="grid-headtext">Tech Stack</p>
-                        <p className="grid-subtext">
-                                       yay
+                        <p className="grid-headtext" >My Passion For Coding</p>
+                        <p className="grid-subtext" style={{ textAlign: "justify" }}>
+                            <p>Coding, for me, is more than just a skill, it's a creative outlet and a means to solve real-world problems. Each project I take on feels like piecing together a puzzle, where the challenges and complexities are part of the thrill. I love the process of turning an idea into something tangible, something functional, and knowing that what I build can make a difference.</p>
+                            <p>I thrive on learning and growing through every challenge I face. Whether it's troubleshooting an issue, optimizing a solution, or exploring new concepts, every step adds to my journey.</p>
+                            <p>Collaboration is another aspect of coding that excites me. I enjoy working alongside others, sharing ideas, and learning from different perspectives.</p>
+                            <p>Ultimately, coding is my way of contributing to the world. It's how I channel my curiosity, creativity, and problem-solving into something meaningful. Each project feels like a step forward, not just for my skills but for the impact I can create through my work.</p>
                         </p>
                     </div>
                </div>
@@ -68,16 +78,19 @@ const About = () => {
                         />
                     </div>
                     <div>
-                        <p className="grid-headtext">I work remotely across most timezones.</p>
-                        <p className="grid-subtext">I'm based in Saudi-Arabia, with remote work available.</p>
-                        <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+                        <p className="grid-headtext">I'm Available To Work Across Any Timezone.</p>
+                        <p className="grid-subtext">I'm based in Saudi-Arabia with available work on-site anywhere in the Eastern Provinence, with remote work available anywhere in the world!</p>
+                        <a href="#contact" className="w-full mt-10">
+                            <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+                        </a>
+
                     </div>
                 </div>
             </div>
 
             <div className="xl:col-span-2 xl:row-span-3">
                 <div className="grid-container">
-                    <img src="assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
+                    <img src="assets/techStack.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
                     <div >
                         <p className="grid-headtext">Tech Stack</p>
                         <p className="grid-subtext">
@@ -85,73 +98,55 @@ const About = () => {
                                 As a versatile developer and data enthusiast, I work across a wide range of technologies, enabling me to build robust applications and handle complex challenges in both software and hardware domains.
                             </p>
 
-                            <h2>Frontend Development</h2>
-                            <ul>
-                                <li>React.js (including React 19)</li>
-                                <li>Vite</li>
-                                <li>Next.js (including Next.js 15)</li>
-                                <li>TailwindCSS</li>
-                                <li>Figma</li>
-                            </ul>
+                            <h2><b>Frontend Development:</b></h2>
+                            <p>
+                                - React.js (including React 19) - Vite - Next.js (including Next.js 15) - TailwindCSS - Figma
+                            </p>
 
-                            <h2>Backend Development</h2>
-                            <ul>
-                                <li>Node.js (Express, Nodemon)</li>
-                                <li>MongoDB</li>
-                                <li>Firebase</li>
-                            </ul>
+                            <h2><b>Backend Development:</b></h2>
+                            <p>
+                                - Node.js (Express) - MongoDB - Firebase - Sanity
+                            </p>
 
-                            <h2>Mobile Development</h2>
-                            <ul>
-                                <li>Flutter</li>
-                            </ul>
+                            <h2><b>Mobile Development:</b></h2>
+                            <p>
+                                - Flutter
+                            </p>
 
-                            <h2>Authentication</h2>
-                            <ul>
-                                <li>NextAuth</li>
-                            </ul>
+                            <h2><b>Authentication and Deployment:</b></h2>
+                            <p>
+                                - NextAuth - Vercel - Render
+                            </p>
 
-                            <h2>Hardware and Embedded Systems</h2>
-                            <ul>
-                                <li>Arduino IDE (ESP32)</li>
-                                <li>C++</li>
-                            </ul>
+                            <h2><b>Hardware and Embedded Systems:</b></h2>
+                            <p>
+                                - Arduino IDE (ESP32) - C++
+                            </p>
 
-                            <h2>Data Handling and Visualization</h2>
-                            <ul>
-                                <li>Pandas</li>
-                                <li>Regex</li>
-                                <li>Excel</li>
-                                <li>Seaborn</li>
-                                <li>Matplotlib</li>
-                            </ul>
+                            <h2><b>Data Handling and Visualization:</b></h2>
+                            <p>
+                                - Pandas - Regex - Excel - Seaborn - Matplotlib
+                            </p>
 
-                            <h2>Version Control and Collaboration</h2>
-                            <ul>
-                                <li>GitHub</li>
-                            </ul>
+                            <h2><b>Version Control and Collaboration:</b></h2>
+                            <p>
+                                - GitHub
+                            </p>
 
-                            <h2>Web Scraping</h2>
-                            <ul>
-                                <li>Selenium</li>
-                                <li>BeautifulSoup4</li>
-                                <li>Google Chrome scraping extension</li>
-                            </ul>
+                            <h2><b>Web Scraping:</b></h2>
+                            <p>
+                                - Selenium - BeautifulSoup4 - Google Chrome scraping extension
+                            </p>
 
-                            <h2>Document Processing</h2>
-                            <ul>
-                                <li>PyPDF</li>
-                                <li>Tabula</li>
-                                <li>Fitz</li>
-                            </ul>
+                            <h2><b>Document Processing:</b></h2>
+                            <p>
+                                - PyPDF - Tabula - Fitz
+                            </p>
 
-                            <h2>Programming Languages</h2>
-                            <ul>
-                                <li>Dart</li>
-                                <li>JavaScript</li>
-                                <li>Python</li>
-                                <li>C++</li>
-                            </ul>
+                            <h2><b>Programming Languages:</b></h2>
+                            <p>
+                                - Dart - JavaScript - Python - C++
+                            </p>
                         </p>
                     </div>
                 </div>
@@ -161,10 +156,14 @@ const About = () => {
                 <div className="grid-container">
                     <img src="assets/grid4.png" alt="grid-4" className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top" />
                     <div className="space-y-2">
-                        <p className="grid-subtext text-center">Contact Me</p>
+                        <p className="grid-subtext text-center">Contact me through my phone number or my email address.</p>
                         <div className="copy-container" onClick={handleCopy}>
                             <img src={hasCopied ? "assets/tick.svg" : "assets/copy.svg"} alt="copy" />
                             <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">norpekbusiness@hotmail.com</p>
+                        </div>
+                        <div className="copy-container" onClick={handleCopy2}>
+                            <img src={hasCopiedNumber ? "assets/tick.svg" : "assets/copy.svg"} alt="copy" />
+                            <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">+966 533 199 759</p>
                         </div>
                     </div>
                 </div>
